e9307b957852f598c002673c1ce60ad053e8bfc5
jharte5 <jonathan.harte@codeimmersives.com>
2019-12-14 18:21:52 -0500

    trying to get the start button to work

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const getPoints = function() {
-    if (results )
-}
+
 
 const fact = document.querySelector('#true')
 const fiction = document.querySelector('#false')
+const riddle = document.querySelector('#questions')
+const factChecker = document.querySelector('#believe-me')
+const pressStart = document.querySelector('#start-nq')
+
+pressStart.addEventListener('click', openingQuestion)
+
+
+const openingQuestion = function() {
+    riddle.innerText = 'Is it ok to awaken a sleepwalker?';
+}
