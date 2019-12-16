ca2d8552efbb6f7c20371714d09a1f1ccc861ef5
jharte5 <jonathan.harte@codeimmersives.com>
2019-12-15 19:29:51 -0500

    added questions to be answered

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-
-
-const fact = document.querySelector('#true')
-const fiction = document.querySelector('#false')
-const riddle = document.querySelector('#questions')
-const factChecker = document.querySelector('#believe-me')
-const pressStart = document.querySelector('#start-nq')
-
-pressStart.addEventListener('click', openingQuestion);
-
-
-const openingQuestion = function() {
-    // let riddle.innerText = 'Is it ok to awaken a sleepwalker?'
-    if  (riddle.innerText === 'Is it ok to awaken a sleepwalker?') {
-    // riddle.innerText = 'Is it ok to awaken a sleepwalker?'
-    // let fact = 
+const questions = [
+    {
+        question: "Is it ok to awaken a sleepwalker?",
+        answers: ["Fact", "Fiction"],
+        correct: 0,
+        description:
+        "They’ll be really confused, but it’s okay. They’re more likely to hurt themselves if they’re not awoken",
+        link: "https://www.sleepfoundation.org/articles/sleepwalking"
+    },
+    {
+        question: "Does Suger affect hyperactivity?",
+        answers: ["Fiction", "Fact"],
+        correct: 0,
+        description:
+        "Studies have disproved this. ADHD and poor behaviours still occurs in children with sugar-free diets",
+        link: "https://www.bmj.com/content/337/bmj.a2769"
+    },
+    {
+        question: "Is closure a real thing?",
+        answers: ["Fact", "Fiction"],
+        correct: 1,
+        description:
+        "No evidence this mythical emotional end state is ever achieved by victims of trauma, bereavement or loss",
+        link: "https://academic.oup.com/ijtj/article/5/1/1/2357002"
+    },
+    {
+        question: "Photogeneic memory is a real thing?",
+        answers: ["Fiction", "Fact"],
+        correct: 0,
+        description:
+        "No hard scientific evidence that a memory that mimics a camera is a real ability. Some people just have great or well-practised memories",
+        link:
+        "https://slate.com/technology/2006/04/no-one-has-a-photographic-memory.html"
+    },
+    {
+        question: "Dont Eat and Swim",
+        answers: ["Fact", "Fiction"],
+        correct: 1,
+        description:
+        "Doesn’t increase risk of cramps; alcohol is the biggest risk increaser. But a full stomachwill make you short of breath",
+        link:
+        "https://www.nytimes.com/2005/06/28/health/the-claim-never-swim-after-eating.html"
     }
-}
+];
