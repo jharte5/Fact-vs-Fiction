8f42a39556429c883de8103d4b998dc11d621e84
jharte5 <jonathan.harte@codeimmersives.com>
2019-12-15 20:38:46 -0500

    were almost there... i think

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 const questions = [
     {
-        question: "Is it ok to awaken a sleepwalker?",
-        answers: ["Fact", "Fiction"],
+        question: 'Is it ok to awaken a sleepwalker?',
+        answers: ['Fact', 'Fiction'],
         correct: 0,
         description:
-        "They’ll be really confused, but it’s okay. They’re more likely to hurt themselves if they’re not awoken",
-        link: "https://www.sleepfoundation.org/articles/sleepwalking"
+        'They’ll be really confused, but it’s okay. They’re more likely to hurt themselves if they’re not awoken',
+        link: 'https://www.sleepfoundation.org/articles/sleepwalking'
     },
     {
-        question: "Does Suger affect hyperactivity?",
-        answers: ["Fiction", "Fact"],
+        question: 'Does Suger affect hyperactivity?',
+        answers: ['Fiction', 'Fact'],
         correct: 0,
         description:
-        "Studies have disproved this. ADHD and poor behaviours still occurs in children with sugar-free diets",
-        link: "https://www.bmj.com/content/337/bmj.a2769"
+        'Studies have disproved this. ADHD and poor behaviours still occurs in children with sugar-free diets',
+        link: 'https://www.bmj.com/content/337/bmj.a2769'
     },
     {
-        question: "Is closure a real thing?",
-        answers: ["Fact", "Fiction"],
+        question: 'Is closure a real thing?',
+        answers: ['Fact', 'Fiction'],
         correct: 1,
         description:
-        "No evidence this mythical emotional end state is ever achieved by victims of trauma, bereavement or loss",
-        link: "https://academic.oup.com/ijtj/article/5/1/1/2357002"
+        'No evidence this mythical emotional end state is ever achieved by victims of trauma, bereavement or loss',
+        link: 'https://academic.oup.com/ijtj/article/5/1/1/2357002'
     },
     {
-        question: "Photogeneic memory is a real thing?",
-        answers: ["Fiction", "Fact"],
+        question: 'Photogeneic memory is a real thing?',
+        answers: ['Fiction', 'Fact'],
         correct: 0,
         description:
-        "No hard scientific evidence that a memory that mimics a camera is a real ability. Some people just have great or well-practised memories",
+        'No hard scientific evidence that a memory that mimics a camera is a real ability. Some people just have great or well-practiced memories',
         link:
-        "https://slate.com/technology/2006/04/no-one-has-a-photographic-memory.html"
+        'https://slate.com/technology/2006/04/no-one-has-a-photographic-memory.html'
     },
     {
-        question: "Dont Eat and Swim",
-        answers: ["Fact", "Fiction"],
+        question: 'Dont Eat and Swim',
+        answers: ['Fact', 'Fiction'],
         correct: 1,
         description:
-        "Doesn’t increase risk of cramps; alcohol is the biggest risk increaser. But a full stomachwill make you short of breath",
+        'Doesn’t increase risk of cramps; alcohol is the biggest risk increaser. But a full stomach will make you short of breath',
         link:
-        "https://www.nytimes.com/2005/06/28/health/the-claim-never-swim-after-eating.html"
+        'https://www.nytimes.com/2005/06/28/health/the-claim-never-swim-after-eating.html'
     }
 ];
 
@@ -119,3 +119,65 @@ const showQuestions = function () {
 }
 
 showQuestions();
+
+// ok so this function checks the answer if its correct or not 
+function checkAnswer() {
+    let selectedItem = document.getElementById(tags);
+    if (selectedItem == undefined) {
+        alert('Please select an answer!');
+        console.log('selected Itemmmmmm', selectedItem);
+        return;
+    } else {
+      // get user answer in form of text
+        const userAns = selectedItem.innerHTML;
+
+      // change the background of the answer according to the Results
+        if (userAns == currentQuestion.answers[currentQuestion.correct]) {
+        console.log('Correct! The answer is: ' + userAns);
+        // change color of selected item by changing className
+        selectedItem.className = 'correct';
+        // count the number of correct answers
+        correctAns++;
+        console.log(correctAns);
+    } else {
+        let showMore = document.createElement('div');
+        showMore.className = 'showMore';
+        let description = document.createElement('p');
+        description.innerHTML = currentQuestion.description;
+        let believe = document.createElement('a');
+        believe.innerHTML = 'dont believe me?';
+        believe.href = currentQuestion.link;
+        believe.setAttribute('target', '_blank');
+        showMore.append(description, believe);
+        ulTag.appendChild(showMore);
+        console.log(
+            'Wrong! The corrent answer is: ' +
+                currentQuestion.answers[currentQuestion.correct]
+        );
+        //change the background of the wrong answer
+        selectedItem.className = 'wrong';
+        //highlight the right answer if the user got it wrong
+        //change the class name of the correct answer
+        ulTag.getElementsByTagName('li')[currentQuestion.correct].className =
+            'correct';
+
+            console.log(currentQuestion.answers[currentQuestion.correct]);
+    }
+
+        // Create a next Question button once the answer has been submitted
+        button.innerHTML = 'Next Question';
+        button.className = 'next';
+        button.id = 'next';
+
+        prevousQuestion = currentQuestion;
+        quizPage++;
+        currentIndex++;
+        currentQuestion = questions[currentIndex];
+
+      // Start with the next question once the "Next" button has been clicked
+        button.onclick = function() {
+        showQuestions();
+        };
+    }
+    return;
+}
